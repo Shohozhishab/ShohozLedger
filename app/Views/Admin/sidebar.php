ad491731b06d9517278d7e63a404195985b05446
dnationsoft12345 <dnationsoftdm8@gmail.com>
2026-03-15 16:14:05 +0600

    some change

--- app/Views/Admin/sidebar.php
+++ app/Views/Admin/sidebar.php
@@ -74,7 +74,6 @@
                     <?php echo add_main_ajax_based_menu_with_permission('Supplier Ledger', '/Admin/Ledger_suppliers', $role_id, 'fa fa-book', '/Admin/Ledger_suppliers_ajax','Ledger_suppliers'); ?>
                     <?php echo add_main_ajax_based_menu_with_permission('Ledger Stock', '/Admin/Ledger_stock', $role_id, 'fa fa-book', '/Admin/Ledger_stock_ajax','Ledger_stock'); ?>
                     <?php echo add_main_ajax_based_menu_with_permission('Ledger Expense', '/Admin/Ledger_expense', $role_id, 'fa fa-book', '/Admin/Ledger_expense_ajax','Ledger_expense'); ?>
-                    <?php echo add_main_ajax_based_menu_with_permission('Ledger Profit', '/Admin/Ledger_profit', $role_id, 'fa fa-book', '/Admin/Ledger_profit_ajax','Ledger_profit'); ?>
                     <?php echo add_main_ajax_based_menu_with_permission('Ledger other sales', '/Admin/Ledger_other_sales', $role_id, 'fa fa-book', '/Admin/Ledger_other_sales_ajax','Ledger_other_sales'); ?>
                 </ul>
             </li>
